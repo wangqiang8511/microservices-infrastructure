973f2ff9e916b2063425aa0f66cb13b00b78f0ed
Chris Aubuchon <Chris.Aubuchon@gmail.com>
2015-06-18 12:11:38 -0500

    Add `secure/` path to Consul K/V.

--- roles/consul/files/consul-acl-bootstrap.sh
+++ roles/consul/files/consul-acl-bootstrap.sh
@@ -4,6 +4,16 @@ set -e
 
 MASTER_TOKEN="$1"
 AGENT_TOKEN="$2"
+SECURE_TOKEN="$3"
 
-curl -X PUT "http://localhost:8500/v1/acl/create?token=$MASTER_TOKEN" \
-    -d "{ \"ID\": \"$AGENT_TOKEN\", \"Name\": \"agent_policy\", \"Type\": \"client\", \"Rules\": \"service \\\"\\\" { policy = \\\"write\\\"}\"}"
+consulacl update --token=${MASTER_TOKEN} --name=agent_policy \
+    --rule='key::write' \
+    --rule='key:secure:deny' \
+    --rule='service::write' \
+    ${AGENT_TOKEN}
+
+consulacl update --token=${MASTER_TOKEN} --name=secure_policy \
+    --rule='key:secure:write' \
+    ${SECURE_TOKEN}
+
+echo ${SECURE_TOKEN} > /etc/consul/private_token
